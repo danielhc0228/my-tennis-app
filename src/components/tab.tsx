7d636c347952f13b82b918ddab26d1700c0ef39d
Daniel Chung <daniel20020228@gmail.com>
2025-07-23 21:07:45 +1000

    modify tab to have a bar at the bottom of pages

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,30 +1,68 @@
-import Link from "next/link";
+"use client";
 
-export default function Tab() {
+import {
+    HomeIcon as SolidHomeIcon,
+    TrophyIcon as SolidTrophyIcon,
+    ClipboardDocumentListIcon as SolidClipboardDocumentListIcon,
+} from "@heroicons/react/24/solid";
+import {
+    HomeIcon as OutlineHomeIcon,
+    TrophyIcon as OutlineTrophyIcon,
+    ClipboardDocumentListIcon as OutlineClipboardDocumentListIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function TabBar() {
+    const pathname = usePathname();
     return (
-        <div className='absolute top-0 right-0 w-64 h-full bg-white shadow-lg z-50 p-8'>
-            <div className='flex flex-col space-y-4'>
-                <Link href={"/"}>
-                    <h1 className='text-gray-700 hover:text-blue-500'>
-                        Overview
-                    </h1>
-                </Link>
-                <Link href={"/league-a"}>
-                    <h1 className='text-gray-700 hover:text-blue-500'>
-                        League A
-                    </h1>
-                </Link>
-                <Link href={"/league-b"}>
-                    <h1 className='text-gray-700 hover:text-blue-500'>
-                        League B
-                    </h1>
-                </Link>
-                <Link href={"/matches"}>
-                    <h1 className='text-gray-700 hover:text-blue-500'>
-                        Matches
-                    </h1>
-                </Link>
-            </div>
+        <div className='fixed bottom-0 mx-auto grid w-full max-w-screen-sm grid-cols-4 border-t border-neutral-600 bg-neutral-800 px-5 py-3 *:text-white'>
+            <Link href='/home' className='flex flex-col items-center gap-px'>
+                {pathname === "/home" ? (
+                    <SolidHomeIcon className='h-7 w-7' />
+                ) : (
+                    <OutlineHomeIcon className='h-7 w-7' />
+                )}
+                <span>Overview</span>
+            </Link>
+            <Link
+                href='/league-a'
+                className='flex flex-col items-center gap-px'
+            >
+                {pathname === "/league-a" ? (
+                    <SolidTrophyIcon className='h-7 w-7' />
+                ) : (
+                    <OutlineTrophyIcon className='h-7 w-7' />
+                )}
+                <span>League A</span>
+            </Link>
+            <Link
+                href='/league-b'
+                className='flex flex-col items-center gap-px'
+            >
+                {pathname === "/league-b" ? (
+                    <SolidTrophyIcon className='h-7 w-7' />
+                ) : (
+                    <OutlineTrophyIcon className='h-7 w-7' />
+                )}
+                <span>League B</span>
+            </Link>
+            <Link href='/matches' className='flex flex-col items-center gap-px'>
+                {pathname === "/matches" ? (
+                    <SolidClipboardDocumentListIcon className='h-7 w-7' />
+                ) : (
+                    <OutlineClipboardDocumentListIcon className='h-7 w-7' />
+                )}
+                <span>Matches</span>
+            </Link>
+            {/* <Link href='/profile' className='flex flex-col items-center gap-px'>
+                {pathname === "/profile" ? (
+                    <SolidUserIcon className='h-7 w-7' />
+                ) : (
+                    <OutlineUserIcon className='h-7 w-7' />
+                )}
+                <span>Profile</span>
+            </Link> */}
         </div>
     );
 }
