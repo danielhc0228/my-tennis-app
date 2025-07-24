fca11a7aaf126651c9ac69df0c70bbb4122ec974
Daniel Chung <daniel20020228@gmail.com>
2025-07-24 13:50:21 +1000

    Visualise wins and losses into green and red circles in tableA

--- src/components/table-wrapper.tsx
+++ src/components/table-wrapper.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
 import TableA from "./table-a";
 import TableB from "./table-b";
 
 export default function TableWrapper() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-            <Suspense fallback={"Loading"}>
-                <TableA />
-                <TableB />
-            </Suspense>
+            <TableA />
+            <TableB />
         </div>
     );
 }
