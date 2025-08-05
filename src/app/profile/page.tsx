e5b7bc4c716a463a2d857f43c3d9e68dfa2814de
Daniel Chung <daniel20020228@gmail.com>
2025-08-05 17:26:25 +1000

    Create profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import db from "@/lib/db";
+
+export default async function PlayerProfiles() {
+    return (
+        <div className='min-h-screen bg-gradient-to-br from-sky-100 to-indigo-100 p-10'>
+            <h1 className='text-4xl font-bold text-center text-indigo-800 mb-10'>
+                Player Profiles
+            </h1>
+        </div>
+    );
+}
--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -4,11 +4,13 @@ import {
     HomeIcon as SolidHomeIcon,
     TrophyIcon as SolidTrophyIcon,
     ClipboardDocumentListIcon as SolidClipboardDocumentListIcon,
+    UserIcon as SolidUserIcon,
 } from "@heroicons/react/24/solid";
 import {
     HomeIcon as OutlineHomeIcon,
     TrophyIcon as OutlineTrophyIcon,
     ClipboardDocumentListIcon as OutlineClipboardDocumentListIcon,
+    UserIcon as OutlineUserIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -16,7 +18,7 @@ import { usePathname } from "next/navigation";
 export default function TabBar() {
     const pathname = usePathname();
     return (
-        <div className='fixed bottom-0 mx-auto grid w-full grid-cols-4 border-t border-neutral-200 bg-white px-5 py-3'>
+        <div className='fixed bottom-0 mx-auto grid w-full grid-cols-5 border-t border-neutral-200 bg-white px-5 py-3'>
             <Link
                 href='/'
                 className={`flex flex-col items-center gap-0.5 ${
@@ -75,14 +77,21 @@ export default function TabBar() {
                 )}
                 <span>Matches</span>
             </Link>
-            {/* <Link href='/profile' className='flex flex-col items-center gap-px'>
+            <Link
+                href='/profile'
+                className={`flex flex-col items-center gap-0.5 ${
+                    pathname === "/matches"
+                        ? "text-blue-600"
+                        : "text-neutral-500"
+                }`}
+            >
                 {pathname === "/profile" ? (
                     <SolidUserIcon className='h-7 w-7' />
                 ) : (
                     <OutlineUserIcon className='h-7 w-7' />
                 )}
                 <span>Profile</span>
-            </Link> */}
+            </Link>
         </div>
     );
 }
