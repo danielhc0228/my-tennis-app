9cde4f11c2086f78ab103d86e8a827e62ab57ebc
Daniel Chung <daniel20020228@gmail.com>
2025-08-05 17:26:59 +1000

    Add player cards in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,46 @@
 import db from "@/lib/db";
+import Image from "next/image";
 
 export default async function PlayerProfiles() {
+    const players = await db.player.findMany({
+        orderBy: [{ league: "asc" }, { name: "asc" }],
+    });
+
     return (
         <div className='min-h-screen bg-gradient-to-br from-sky-100 to-indigo-100 p-10'>
             <h1 className='text-4xl font-bold text-center text-indigo-800 mb-10'>
                 Player Profiles
             </h1>
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-6xl mx-auto'>
+                {players.map((player) => (
+                    <div
+                        key={player.id}
+                        className='bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition duration-300 flex flex-col items-center text-center'
+                    >
+                        <Image
+                            // src={`https://api.dicebear.com/8.x/thumbs/svg?seed=${player.name}`}
+                            src={"/avatar.png"}
+                            alt={`${player.name}'s avatar`}
+                            className='w-24 h-24 rounded-full mb-4'
+                            width={50}
+                            height={50}
+                        />
+                        <h2 className='text-xl font-semibold text-gray-800'>
+                            {player.name}
+                        </h2>
+                        <p
+                            className={`mt-1 text-sm font-medium ${
+                                player.league === "A"
+                                    ? "text-green-600"
+                                    : "text-blue-600"
+                            }`}
+                        >
+                            League {player.league}
+                        </p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
