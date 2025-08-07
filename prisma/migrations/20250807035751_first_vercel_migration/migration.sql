42d9c7566391837db103c745fa18a33b07caf971
Daniel Chung <daniel20020228@gmail.com>
2025-08-07 13:58:29 +1000

    Add new migration folder for vercel deployment

---
+++ prisma/migrations/20250807035751_first_vercel_migration/migration.sql
@@ -0,0 +1,85 @@
+-- CreateEnum
+CREATE TYPE "League" AS ENUM ('A', 'B');
+
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "league" "League" NOT NULL,
+    "seasonWins" INTEGER NOT NULL DEFAULT 0,
+    "seasonLosses" INTEGER NOT NULL DEFAULT 0,
+    "totalWins" INTEGER NOT NULL DEFAULT 0,
+    "totalLosses" INTEGER NOT NULL DEFAULT 0,
+    "seasonPoints" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "power" INTEGER NOT NULL DEFAULT 0,
+    "serve" INTEGER NOT NULL DEFAULT 0,
+    "accuracy" INTEGER NOT NULL DEFAULT 0,
+    "agility" INTEGER NOT NULL DEFAULT 0,
+    "volley" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Match" (
+    "id" SERIAL NOT NULL,
+    "player1Id" INTEGER NOT NULL,
+    "player2Id" INTEGER NOT NULL,
+    "winnerId" INTEGER NOT NULL,
+    "player1Score" INTEGER NOT NULL,
+    "player2Score" INTEGER NOT NULL,
+    "season" INTEGER NOT NULL,
+    "matchDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Match_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Config" (
+    "id" SERIAL NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Config_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Season" (
+    "id" SERIAL NOT NULL,
+    "number" INTEGER NOT NULL,
+    "winnerAId" INTEGER NOT NULL,
+    "loserAId" INTEGER NOT NULL,
+    "winnerBId" INTEGER NOT NULL,
+    "loserBId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Season_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Match_season_idx" ON "Match"("season");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Config_key_key" ON "Config"("key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Season_number_key" ON "Season"("number");
+
+-- AddForeignKey
+ALTER TABLE "Match" ADD CONSTRAINT "Match_player1Id_fkey" FOREIGN KEY ("player1Id") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Match" ADD CONSTRAINT "Match_player2Id_fkey" FOREIGN KEY ("player2Id") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_winnerAId_fkey" FOREIGN KEY ("winnerAId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_loserAId_fkey" FOREIGN KEY ("loserAId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_winnerBId_fkey" FOREIGN KEY ("winnerBId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_loserBId_fkey" FOREIGN KEY ("loserBId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
